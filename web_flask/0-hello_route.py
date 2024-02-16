d4444a8e19fbdfa049990269bd0e76ee0fa2ec57
Yahya <yahyaoulha0@gmail.com>
2024-02-16 15:22:33 +0100

    maybe?

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+""" Flask Basic WebApp """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def main():
+    """ return a string """
+    return "Hello HBNB!"
+
+
+app.run(port=5000)
